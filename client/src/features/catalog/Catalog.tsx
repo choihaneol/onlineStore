09eeb1ed86b0019e93c9f745311aed08b870c613
choihaneol <chu1004a@naver.com>
2022-07-19 14:15:45 -0400

    axios congifuration

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,6 +1,7 @@
 import ProductList from "./ProductList";
 import { Product } from "../../app/models/product";
 import { useState, useEffect } from "react";
+import agent from "../../app/api/agents";
 
 
 export default function Catalog() {
@@ -8,9 +9,7 @@ export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]); //useState : component에 state변수와 state 추가
 
   useEffect(() => { //useEffect : 값의 변화가 일어났을때 실행
-    fetch('http://localhost:5000/api/products') //for header
-      .then(response => response.json())
-      .then(data => setProducts(data))
+    agent.Catalog.list().then(products => setProducts(products))
   }, []) //렌더링될때 한번만 실행 
 
 
